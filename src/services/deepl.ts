127e3a3369d4fa2b7c9aad2af75481ae2173214e
Leo Bernard <admin@leolabs.org>
2019-03-07 21:47:59 +0100

    :sparkles: Add DeepL translation service

---
+++ src/services/deepl.ts
@@ -0,0 +1,71 @@
+import fetch from 'node-fetch';
+
+import { TranslationService, TranslationResult } from '.';
+import { replaceIcu, reInsertIcu } from '../icu';
+
+const API_ENDPOINT = 'https://api.deepl.com/v2';
+
+export class DeepL implements TranslationService {
+  public name = 'DeepL';
+  private apiKey: string;
+
+  initialize(config?: string) {
+    if (!config) {
+      throw new Error(`Please provide an API key for DeepL.`);
+    }
+
+    this.apiKey = config;
+  }
+
+  async getAvailableLanguages() {
+    return ['en', 'de', 'fr', 'es', 'pt', 'it', 'nl', 'pl', 'ru'];
+  }
+
+  async translateStrings(
+    strings: { key: string; value: string }[],
+    from: string,
+    to: string,
+  ) {
+    return Promise.all(
+      strings.map(string => this.translateString(string, from, to)),
+    );
+  }
+
+  async translateString(
+    string: { key: string; value: string },
+    from: string,
+    to: string,
+    triesLeft: number = 5,
+  ): Promise<TranslationResult> {
+    const { clean, replacements } = replaceIcu(string.value);
+
+    const url = new URL(`${API_ENDPOINT}/translate`);
+    url.searchParams.append('text', clean);
+    url.searchParams.append('source_lang', from.toUpperCase());
+    url.searchParams.append('target_lang', to.toUpperCase());
+    url.searchParams.append('auth_key', this.apiKey);
+
+    const response = await fetch(String(url));
+
+    if (!response.ok) {
+      if (response.status === 429 && triesLeft > 0) {
+        return this.translateString(string, from, to, triesLeft - 1);
+      }
+
+      throw new Error(
+        `[${response.status} ${
+          response.statusText
+        }]: ${(await response.text()) || 'Empty body'}`,
+      );
+    }
+
+    return {
+      key: string.key,
+      value: string.value,
+      translated: reInsertIcu(
+        (await response.json()).translations[0].text,
+        replacements,
+      ),
+    };
+  }
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import { GoogleTranslate } from './google-translate';
+import { DeepL } from './deepl';
 import { DryRun } from './dry-run';
 import { ManualTranslation } from './manual';
 
@@ -22,6 +23,7 @@ export const serviceMap: {
   [k: string]: TranslationService;
 } = {
   'google-translate': new GoogleTranslate(),
+  deepl: new DeepL(),
   'dry-run': new DryRun(),
   manual: new ManualTranslation(),
 };
