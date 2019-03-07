ea9ae27d9d94c7bfa6527bc2f594e04ff53c1c8c
Leo Bernard <admin@leolabs.org>
2019-03-07 14:59:03 +0100

    :recycle: Extract ICU-related functions into separate file for easier re-use

---
+++ src/icu.ts
@@ -0,0 +1,37 @@
+import { parse } from 'messageformat-parser';
+
+export const generateSearchRegex = (input: string) => {
+  const parts = parse(input);
+
+  return new RegExp(
+    parts
+      .map((part: string) =>
+        typeof part === 'string'
+          ? part.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+          : '(.+?)',
+      )
+      .join(''),
+  );
+};
+
+export const replaceIcu = (input: string) => {
+  const searchRegex = generateSearchRegex(input);
+  const matches = input.match(searchRegex);
+
+  const replacements = matches.slice(1).map((match, index) => ({
+    from: match,
+    to: `{${index}}`,
+  }));
+
+  const clean = replacements.reduce(
+    (acc, cur) => acc.replace(cur.from, cur.to),
+    input,
+  );
+
+  return { clean, replacements };
+};
+
+export const reInsertIcu = (
+  clean: string,
+  replacements: { from: string; to: string }[],
+) => replacements.reduce((acc, cur) => acc.replace(cur.to, cur.from), clean);
--- src/services/google-translate.ts
+++ src/services/google-translate.ts
@@ -1,43 +1,7 @@
 import { Translate } from '@google-cloud/translate';
-import { parse } from 'messageformat-parser';
+import { replaceIcu, reInsertIcu } from '../icu';
 
 const translate = new Translate({ autoRetry: true });
-
-export const generateSearchRegex = (input: string) => {
-  const parts = parse(input);
-
-  return new RegExp(
-    parts
-      .map((part: string) =>
-        typeof part === 'string'
-          ? part.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
-          : '(.+?)',
-      )
-      .join(''),
-  );
-};
-
-export const replaceIcu = (input: string) => {
-  const searchRegex = generateSearchRegex(input);
-  const matches = input.match(searchRegex);
-
-  const replacements = matches.slice(1).map((match, index) => ({
-    from: match,
-    to: `{${index}}`,
-  }));
-
-  const clean = replacements.reduce(
-    (acc, cur) => acc.replace(cur.from, cur.to),
-    input,
-  );
-
-  return { clean, replacements };
-};
-
-export const reInsertIcu = (
-  clean: string,
-  replacements: { from: string; to: string }[],
-) => replacements.reduce((acc, cur) => acc.replace(cur.to, cur.from), clean);
 
 export const translateStrings = async (
   strings: { key: string; value: string }[],
