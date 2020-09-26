de258ff699f2274226d0f116bae8d861aa5b0ea5
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-26 18:09:18 +0200

    Hello world

--- test/translate-core/translate-core.test.ts
+++ test/translate-core/translate-core.test.ts
@@ -5,30 +5,45 @@ import {
 } from "../../src/core/translate-core";
 import { TSet } from "../../src/core/core-definitions";
 
-const commonArgs: Omit<CoreArgs, "src" | "oldTarget" | "oldSrcCache"> = {
+const commonArgs: Omit<
+  CoreArgs,
+  "src" | "oldTarget" | "oldSrcCache" | "targetLng"
+> = {
   srcLng: "en",
-  targetLng: "de",
   service: "google-translate", // TODO: Type safety
   serviceConfig: "gcloud/gcloud_service_account.json",
   matcher: "icu", // TODO: Type safety
 };
 
-test("simpleTranslate", async () => {
-  const src: TSet = {
-    translations: [{ key: "hello", value: "Hello" }],
-  };
-  const args: CoreArgs = {
-    src,
-    oldTarget: null,
-    oldSrcCache: null,
-    ...commonArgs,
-  };
-  const expectRes: CoreResults = {
-    newTarget: {
-      translations: [{ key: "hello", value: "Hallo" }],
-    },
-    newSrcCache: null,
-  };
-  const res = await translateCore(args);
-  expect(res).toStrictEqual(expectRes);
+interface LngT {
+  lng: string;
+  t: string;
+}
+
+describe.each([
+  { lng: "de", t: "Hallo" },
+  { lng: "es", t: "Hola" },
+  { lng: "fr", t: "Bonjour" },
+  { lng: "it", t: "Ciao" },
+])("Hello %s", (lngT: LngT) => {
+  test("Hello world - no cache", async () => {
+    const src: TSet = {
+      translations: [{ key: "hello", value: "Hello" }],
+    };
+    const args: CoreArgs = {
+      src,
+      oldTarget: null,
+      oldSrcCache: null,
+      targetLng: lngT.lng,
+      ...commonArgs,
+    };
+    const expectRes: CoreResults = {
+      newTarget: {
+        translations: [{ key: "hello", value: lngT.t }],
+      },
+      newSrcCache: null,
+    };
+    const res = await translateCore(args);
+    expect(res).toStrictEqual(expectRes);
+  });
 });
