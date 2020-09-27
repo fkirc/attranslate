405b1cc906a202e1a64224113ad45f258194ff24
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-27 23:33:28 +0200

    git diff

--- sample-scripts/flat_json.sh
+++ sample-scripts/flat_json.sh
@@ -2,3 +2,5 @@
 set -x
 
 ./bin/attranslate --srcFile='test-assets/flat-json/count-en.flat.json' --srcLng='en' --targetFile='test-assets/flat-json/count-de.flat.json' --targetLng='de' --serviceConfig='gcloud/gcloud_service_account.json'
+
+git diff --exit-code # Remove this line if you are not running automated tests.
