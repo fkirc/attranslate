3d423e25f7fdb31c5df084cc58d3c42aa2a7eea5
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-27 20:42:05 +0200

    e2e

--- test/e2e/e2e-simple.test.ts
+++ test/e2e/e2e-simple.test.ts
@@ -1,10 +1,22 @@
-import { runTranslate } from "../test-util";
+import { runCommand, runTranslate } from "../test-util";
 
-const helloEn = "test-assets/hello-en-flat.json";
-const helloDe = "test-assets/hello-de-flat.json";
-const commonArgs = `--srcFile='${helloEn}' --srcLng='en' --targetFile='${helloDe}' --targetLng='de' --serviceConfig='gcloud/gcloud_service_account.json'`;
+describe.each([
+  {
+    src: "test-assets/hello-en-flat.json",
+    target: "test-assets/hello-de-flat.json",
+  },
+])("simple translate", (args) => {
+  const commonArgs = `--srcFile='${args.src}' --srcLng='en' --targetFile='${args.target}' --targetLng='de' --serviceConfig='gcloud/gcloud_service_account.json'`;
 
-test("up-to-date cache, up-to-date target", async () => {
-  const output = await runTranslate(commonArgs);
-  expect(output).toBe("Nothing changed, translations are up-to-date.\n");
+  test("up-to-date cache, up-to-date target", async () => {
+    const output = await runTranslate(commonArgs);
+    expect(output).toBe("Nothing changed, translations are up-to-date.\n");
+  });
+
+  test("up-to-date cache, missing target", async () => {
+    await runCommand(`rm ${args.target}`);
+    const output = await runTranslate(commonArgs);
+    expect(output).toContain("Add 3 new translations");
+    expect(output).toContain("Write target-file");
+  });
 });
