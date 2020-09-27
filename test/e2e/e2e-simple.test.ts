9d07c2b3f53f1aade44c52e757b70758044b97f5
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-27 20:10:46 +0200

    flat JSON

--- test/e2e/e2e-simple.test.ts
+++ test/e2e/e2e-simple.test.ts
@@ -1,7 +1,7 @@
 import { runTranslate } from "../test-util";
 
-const helloEn = "test-assets/hello-en.json";
-const helloDe = "test-assets/hello-de.json";
+const helloEn = "test-assets/hello-en-flat.json";
+const helloDe = "test-assets/hello-de-flat.json";
 const commonArgs = `--srcFile='${helloEn}' --srcLng='en' --targetFile='${helloDe}' --targetLng='de' --serviceConfig='gcloud/gcloud_service_account.json'`;
 
 test("up-to-date cache, up-to-date target", async () => {
