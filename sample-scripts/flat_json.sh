100d96db59c9f04eb5f35ce225fce079bd4b9d2c
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-30 16:04:19 +0200

    Fix sample test

--- sample-scripts/flat_json.sh
+++ sample-scripts/flat_json.sh
@@ -2,5 +2,3 @@
 set -xe # Print everything, abort if anything fails
 
 ./bin/attranslate --srcFile='test-assets/flat-json/count-en.flat.json' --srcLng='en' --srcFormat='flat-json' --targetFile='test-assets/flat-json/count-de.flat.json' --targetLng='de' --targetFormat='flat-json' --service='google-translate' --serviceConfig='gcloud/gcloud_service_account.json'
-
-git diff --exit-code # Remove this line if you are not running automated tests.
--- test/e2e/sample-scripts.test.ts
+++ test/e2e/sample-scripts.test.ts
@@ -3,4 +3,5 @@ import { runCommand } from "../test-util";
 test("flat_json", async () => {
   const output = await runCommand(`./sample-scripts/flat_json.sh`);
   expect(output).toBe("Nothing changed, translations are up-to-date.\n");
+  await runCommand(`git diff --exit-code test-assets/`);
 });
