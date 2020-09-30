57b9a7f16acfe91ce17238ab8003ad0c356aebce
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-30 15:26:47 +0200

    Test fix

--- test/e2e/sample-scripts.test.ts
+++ test/e2e/sample-scripts.test.ts
@@ -2,6 +2,5 @@ import { runCommand } from "../test-util";
 
 test("flat_json", async () => {
   const output = await runCommand(`./sample-scripts/flat_json.sh`);
-  expect(output).toContain("Add 3 new translations");
   expect(output).toBe("Nothing changed, translations are up-to-date.\n");
 });
