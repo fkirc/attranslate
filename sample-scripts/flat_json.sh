0e02f70bd8595fdab0227d040b0061ac74077356
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-01 12:35:48 +0200

    Make sample script independent of other test assets

--- sample-scripts/flat_json.sh
+++ sample-scripts/flat_json.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -xe # Print everything, abort if anything fails
 
-./bin/attranslate --srcFile='test-assets/flat-json/count-en.flat.json' --srcLng='en' --srcFormat='flat-json' --targetFile='test-assets/flat-json/count-de.flat.json' --targetLng='de' --targetFormat='flat-json' --service='google-translate' --serviceConfig='gcloud/gcloud_service_account.json' --cacheDir='test-assets/'
+attranslate='../bin/attranslate'
+
+$attranslate --srcFile='en/count.json' --srcLng='en' --srcFormat='flat-json' --targetFile='de/count.json' --targetLng='de' --targetFormat='flat-json' --service='google-translate' --serviceConfig='gcloud/gcloud_service_account.json' --cacheDir='translate-cache'
