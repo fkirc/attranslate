e5f522f6080f57c3eb839f2a7648aaf1cbee1e98
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-01 12:41:39 +0200

    Update sample

--- test/e2e/sample-scripts.test.ts
+++ test/e2e/sample-scripts.test.ts
@@ -1,7 +1,7 @@
 import { runCommand } from "../test-util";
 
 test("flat_json", async () => {
-  const output = await runCommand(`./sample-scripts/flat_json.sh`);
+  const output = await runCommand(`./flat_json.sh`, "sample-scripts");
   expect(output).toBe("Nothing changed, translations are up-to-date.\n");
   await runCommand(`git diff --exit-code test-assets/`);
 });
