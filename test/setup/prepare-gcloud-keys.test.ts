36e39f486710cba92439cc820de18bccccc6e4d6
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-06 13:26:26 +0200

    prepservice

--- test/setup/prepare-gcloud-keys.test.ts
+++ test/setup/prepare-gcloud-keys.test.ts
@@ -1,10 +1,14 @@
-import { readJsonFile } from "../../src/util/util";
+import { logFatal, readJsonFile, writeJsonFile } from "../../src/util/util";
 import { GCloudKeyFile } from "../../src/services/google-translate";
 
 test("setupGcloudPrivateKey", () => {
-  //const privateKey = "jioo";
-  const serviceAccount = readJsonFile<GCloudKeyFile>(
-    "gcloud/gcloud_service_account.json"
-  );
+  const privateKey = process.env.GCLOUD_PRIVATE_KEY;
+  if (!privateKey) {
+    logFatal("Did not find GCLOUD_PRIVATE_KEY");
+  }
+  const serviceAccountPath = "gcloud/gcloud_service_account.json";
+  const serviceAccount = readJsonFile<GCloudKeyFile>(serviceAccountPath);
   expect(serviceAccount.private_key).toContain("BEGIN PRIVATE KEY");
+  serviceAccount.private_key = privateKey.split("\\n").join("\n");
+  writeJsonFile(serviceAccountPath, serviceAccount);
 });
