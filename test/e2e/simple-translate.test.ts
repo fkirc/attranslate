4d7903d1bd177767baba0d8ba1b69bd23c4a8655
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-07 22:10:58 +0200

    rf outdated cache

--- test/e2e/simple-translate.test.ts
+++ test/e2e/simple-translate.test.ts
@@ -1,13 +1,17 @@
-import { runCommand, runTranslate } from "../test-util/test-util";
+import {
+  runCommand,
+  runCommandExpectFailure,
+  runTranslate,
+} from "../test-util/test-util";
 import { buildE2EArgs, defaultE2EArgs } from "./e2e-common";
 import { join } from "path";
 import { getDebugPath, readJsonFile, writeJsonFile } from "../../src/util/util";
 import { CliArgs } from "../../src/core/core-definitions";
 const cacheDir = "test-assets/cache/";
-const cacheOutdatedDir = "test-assets/cache-outdated/";
+const cacheDirOutdated = "test-assets/cache-outdated/";
 const cacheMissingDir = "test-assets/cache-missing/";
 
-const testArgs: Partial<CliArgs>[] = [
+const testArray: Partial<CliArgs>[] = [
   {
     srcFile: "test-assets/flat-json/count-en.flat.json",
     srcFormat: "flat-json",
@@ -22,7 +26,74 @@ const testArgs: Partial<CliArgs>[] = [
   },
 ];
 
-describe.each(testArgs)("translate %p", (args) => {
+async function preMissingTarget(args: CliArgs) {
+  await runCommand(`rm ${args.targetFile}`);
+}
+
+async function postMissingTarget(args: CliArgs, output: string) {
+  expect(output).toContain(`Add 3 new translations`);
+  expect(output).toContain(
+    `Write target-file ${getDebugPath(args.targetFile)}`
+  );
+  await runCommand(`git diff --exit-code ${args.targetFile}`);
+}
+
+async function preCleanTarget(args: CliArgs) {
+  await runCommand(`git diff --exit-code ${args.targetFile}`);
+}
+
+async function postCleanTarget(args: CliArgs) {
+  await runCommand(`git diff --exit-code ${args.targetFile}`);
+}
+
+async function preModifiedTarget(args: CliArgs) {
+  modifyFirstTwoProperties(args.targetFile);
+  await runCommandExpectFailure(`git diff --exit-code ${args.targetFile}`);
+}
+
+async function postModifiedTarget(args: CliArgs) {
+  await runCommand(`git checkout ${args.targetFile}`);
+}
+
+describe.each(testArray)("outdated cache %p", (commonArgs) => {
+  const args: CliArgs = {
+    ...defaultE2EArgs,
+    ...commonArgs,
+    cacheDir: cacheDirOutdated,
+  };
+
+  async function runWithOutdatedCache(): Promise<string> {
+    const output = await runTranslate(buildE2EArgs(args));
+    expect(output).toContain(`Write cache`);
+    await runCommandExpectFailure(`git diff --exit-code ${args.cacheDir}`);
+    await runCommand(`git checkout ${args.cacheDir}`);
+    return output;
+  }
+
+  test("missing target", async () => {
+    await preMissingTarget(args);
+    const output = await runWithOutdatedCache();
+    await postMissingTarget(args, output);
+  });
+
+  test("clean target", async () => {
+    await preCleanTarget(args);
+    await runWithOutdatedCache();
+    await postCleanTarget(args);
+  });
+
+  test("modified target", async () => {
+    await preModifiedTarget(args);
+    const output = await runWithOutdatedCache();
+    expect(output).toContain("Update 1 existing translations\n");
+    expect(output).toContain(
+      `Write target-file ${getDebugPath(args.targetFile)}`
+    );
+    await postModifiedTarget(args);
+  });
+});
+
+describe.each(testArray)("translate %p", (args) => {
   const commonArgs: CliArgs = {
     ...defaultE2EArgs,
     ...args,
@@ -45,38 +116,6 @@ describe.each(testArgs)("translate %p", (args) => {
     const output = await runTranslate(buildE2EArgs(commonArgs));
     expect(output).toBe("Nothing changed, translations are up-to-date.\n");
     await runCommand(`git checkout ${args.targetFile}`);
-  });
-
-  const outdatedCacheArgs: CliArgs = {
-    ...commonArgs,
-    cacheDir: cacheOutdatedDir,
-  };
-  test("outdated cache, missing target", async () => {
-    await runCommand(`rm ${args.targetFile}`);
-    const output = await runTranslate(buildE2EArgs(outdatedCacheArgs));
-    expect(output).toContain(`Add 3 new translations`);
-    expect(output).toContain(`Write cache`);
-    expect(output).toContain(cacheOutdatedDir);
-    await runCommand(`git checkout ${cacheOutdatedDir}`);
-  });
-
-  test("outdated cache, clean target", async () => {
-    const output = await runTranslate(buildE2EArgs(outdatedCacheArgs));
-    expect(output).toContain(`Write cache`);
-    expect(output).toContain(cacheOutdatedDir);
-    await runCommand(`git checkout ${cacheOutdatedDir}`);
-  });
-
-  test("outdated cache, modified target", async () => {
-    modifyFirstTwoProperties(commonArgs.targetFile);
-    const output = await runTranslate(buildE2EArgs(outdatedCacheArgs));
-    expect(output).toContain("Update 1 existing translations\n");
-    expect(output).toContain(
-      `Write target-file ${getDebugPath(commonArgs.targetFile)}`
-    );
-    expect(output).toContain(`Write cache`);
-    await runCommand(`git checkout ${cacheOutdatedDir}`);
-    await runCommand(`git checkout ${commonArgs.targetFile}`);
   });
 
   const missingCacheArgs: CliArgs = {
