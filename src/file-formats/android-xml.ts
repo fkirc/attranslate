2fefd69e79634c6a421b87c86578aec32ab6e485
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-12 15:16:23 +0200

    writeTFile Android XML

--- src/file-formats/android-xml.ts
+++ src/file-formats/android-xml.ts
@@ -4,8 +4,13 @@ import {
   WriteTFileArgs,
 } from "./file-format-definitions";
 import { TSet } from "../core/core-definitions";
-import { getDebugPath, logFatal, readUtf8File } from "../util/util";
-import { toJson } from "xml2json";
+import {
+  getDebugPath,
+  logFatal,
+  readUtf8File,
+  writeUf8File,
+} from "../util/util";
+import { toJson, toXml } from "xml2json";
 
 interface AndroidResourceFile {
   resources: {
@@ -18,6 +23,9 @@ interface StringResource {
   formatted?: string;
   $t: string;
 }
+
+const ANDROID_KEY_SEPARATOR = "_";
+const JSON_KEY_SEPARATOR = ".";
 
 export class AndroidXml implements TFileFormat {
   readTFile(args: ReadTFileArgs): TSet {
@@ -39,31 +47,45 @@ export class AndroidXml implements TFileFormat {
       if (!rawKey) {
         logXmlError(`undefined key: '${stringResource}'`, args);
       }
-      const key = rawKey.split("_").join(".");
+      const key = rawKey.split(ANDROID_KEY_SEPARATOR).join(JSON_KEY_SEPARATOR);
       tSet.set(key, value ?? null);
     });
     return tSet;
   }
 
   writeTFile(args: WriteTFileArgs): void {
-    logFatal("Not implemented");
+    const resources: StringResource[] = [];
+    args.tSet.forEach((value, jsonKey) => {
+      const androidKey = jsonKey
+        .split(JSON_KEY_SEPARATOR)
+        .join(ANDROID_KEY_SEPARATOR);
+      resources.push({
+        name: androidKey,
+        formatted: "false",
+        $t: value ?? "",
+      });
+    });
+    const resourceFile: AndroidResourceFile = {
+      resources: {
+        string: resources,
+      },
+    };
+    const jsonString = JSON.stringify(resourceFile);
+    const xmlString = toXml(jsonString, { sanitize: true });
+    writeUf8File(args.path, xmlString);
   }
 }
 
 function parseRawXML<T>(args: ReadTFileArgs): Partial<T> {
   const xmlString = readUtf8File(args.path);
-  let jsonString;
   try {
-    jsonString = toJson(xmlString);
+    return (toJson(xmlString, {
+      object: true,
+      sanitize: true,
+    }) as unknown) as Partial<T>;
   } catch (e) {
     console.error(e);
     logXmlError("XML parsing error", args);
-  }
-  try {
-    return JSON.parse(jsonString);
-  } catch (e) {
-    console.error(e);
-    logXmlError("Failed to convert XML into JSON", args);
   }
 }
 
