55f4f37fd966ef830feb54673f0cb9e271a789ee
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-12 17:20:23 +0200

    Remove ignored service config

--- sample-scripts/simple_translate.sh
+++ sample-scripts/simple_translate.sh
@@ -3,4 +3,4 @@ set -e # Abort if anything fails
 
 attranslate='../bin/attranslate'
 
-$attranslate --srcFile='en/fruits.json' --srcLng='en' --srcFormat='nested-json' --targetFile='de/fruits.json' --targetLng='de' --targetFormat='nested-json' --service='manual' --serviceConfig='ignored if service=manual'
+$attranslate --srcFile='en/fruits.json' --srcLng='en' --srcFormat='nested-json' --targetFile='de/fruits.json' --targetLng='de' --targetFormat='nested-json' --service='manual'
