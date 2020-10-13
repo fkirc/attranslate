4677905cce296dad520371602891dfdc0aa9c10e
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-13 15:01:39 +0200

    Remove npx

--- sample-scripts/multi_translate.sh
+++ sample-scripts/multi_translate.sh
@@ -5,7 +5,7 @@ CACHE_DIR="translate-cache"
 SERVICE_ACCOUNT_KEY="../gcloud/gcloud_service_account.json"
 COMMON_ARGS=( "--srcFile=en/fruits.json" "--srcLng=en" "--srcFormat=nested-json" "--targetFormat=nested-json" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=$CACHE_DIR" "--matcher=i18next" )
 
-# You can remove "npx" if you install it via: npm install --global attranslate
-npx attranslate "${COMMON_ARGS[@]}" --targetFile='es/fruits.json' --targetLng='es'
-npx attranslate "${COMMON_ARGS[@]}" --targetFile='zh/fruits.json' --targetLng='zh'
-npx attranslate "${COMMON_ARGS[@]}" --targetFile='de/fruits.json' --targetLng='de'
+# Run "npm install --global attranslate" before you try this example
+attranslate "${COMMON_ARGS[@]}" --targetFile='es/fruits.json' --targetLng='es'
+attranslate "${COMMON_ARGS[@]}" --targetFile='zh/fruits.json' --targetLng='zh'
+attranslate "${COMMON_ARGS[@]}" --targetFile='de/fruits.json' --targetLng='de'
--- sample-scripts/simple_translate.sh
+++ sample-scripts/simple_translate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-# You can remove "npx" if you install it via: npm install --global attranslate
-npx attranslate --srcFile='en/fruits.json' --srcLng='en' --srcFormat='nested-json' --targetFile='de/fruits.json' --targetLng='de' --targetFormat='nested-json' --service='manual'
+# Run "npm install --global attranslate" before you try this example
+attranslate --srcFile='en/fruits.json' --srcLng='en' --srcFormat='nested-json' --targetFile='de/fruits.json' --targetLng='de' --targetFormat='nested-json' --service='manual'
