d9172b1abb3e0e83d4d41d3de0901d46d1563eeb
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-13 15:59:41 +0200

    Android to iOS re-create targets

--- test/e2e/sample-script-android-to-ios.test.ts
+++ test/e2e/sample-script-android-to-ios.test.ts
@@ -1,15 +1,36 @@
 import { joinLines } from "../test-util/test-util";
-import { runSampleScript } from "./sample-scripts-util";
+import { runSampleScript, sampleDir } from "./sample-scripts-util";
+import { unlinkSync } from "fs";
+import { join } from "path";
+import { getDebugPath } from "../../src/util/util";
+
+const targetPaths: string[] = [
+  "android/app/src/main/res/values-de/strings.xml",
+  "android/app/src/main/res/values-es/strings.xml",
+  "ios/Localizable/Base.lproj/Localizable.strings",
+  "ios/Localizable/de.lproj/Localizable.strings",
+  "ios/Localizable/es.lproj/Localizable.strings",
+];
 
 test("Android to iOS clean", async () => {
   const output = await runSampleScript(`./android_to_ios.sh`);
   expect(output).toBe(
-    joinLines([
-      "Target is up-to-date: 'android/app/src/main/res/values-de/strings.xml'",
-      "Target is up-to-date: 'android/app/src/main/res/values-es/strings.xml'",
-      "Target is up-to-date: 'ios/Localizable/Base.lproj/Localizable.strings'",
-      "Target is up-to-date: 'ios/Localizable/de.lproj/Localizable.strings'",
-      "Target is up-to-date: 'ios/Localizable/es.lproj/Localizable.strings'",
-    ])
+    joinLines(
+      targetPaths.map((path) => {
+        return `Target is up-to-date: '${path}'`;
+      })
+    )
   );
+});
+
+test("Android to iOS re-create targets", async () => {
+  targetPaths.forEach((path) => {
+    unlinkSync(join(sampleDir, path));
+  });
+  const output = await runSampleScript(`./android_to_ios.sh`);
+  targetPaths.forEach((path) => {
+    expect(output).toContain(
+      `Write target ${getDebugPath(join(sampleDir, path))}`
+    );
+  });
 });
