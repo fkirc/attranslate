d8d70a300969bef65389d2116cd91ec42a6544bb
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-13 02:48:15 +0200

    Clean targets

--- test/e2e/translate-outdated-cache.test.ts
+++ test/e2e/translate-outdated-cache.test.ts
@@ -19,29 +19,31 @@ const cacheDirOutdated = join("test-assets", "cache-outdated");
 
 const outdatedCacheTests: {
   args: E2EArgs;
+  cleanTargetFile: string;
   maxTime: number;
 }[] = [
   {
     args: {
       ...defaultE2EArgs,
-      srcFile: "test-assets/flat-json/count-empty-null.flat.json",
+      srcFile: "test-assets/flat-json/count-empty-null.json",
       srcFormat: "flat-json",
-      targetFile: "test-assets/flat-json/count-empty.flat.modified.json",
-      refTargetFile:
-        "test-assets/flat-json/count-empty.flat.less-modified.json",
+      targetFile: "test-assets/flat-json/count-empty.modified.json",
+      refTargetFile: "test-assets/flat-json/count-empty.less-modified.json",
       targetFormat: "nested-json",
     },
+    cleanTargetFile: "test-assets/flat-json/count-empty.clean.json",
     maxTime: offlineMaxTime,
   },
   {
     args: {
       ...defaultE2EArgs,
-      srcFile: "test-assets/nested-json/count-en.nested.json",
+      srcFile: "test-assets/nested-json/count-en.json",
       srcFormat: "nested-json",
-      targetFile: "test-assets/nested-json/count-de.flattened.modified.json",
-      refTargetFile: "test-assets/nested-json/count-de.flattened.json",
+      targetFile: "test-assets/nested-json/count-de.modified.json",
+      refTargetFile: "test-assets/nested-json/count-de.less-modified.json",
       targetFormat: "flat-json",
     },
+    cleanTargetFile: "test-assets/nested-json/count-de.clean.json",
     maxTime: onlineMaxTime,
   },
 ];
@@ -64,6 +66,8 @@ describe.each(outdatedCacheTests)("outdated cache %p", (commonArgs) => {
 
   test("missing target", async () => {
     const args = { ...argsTemplate };
+    args.targetFile = commonArgs.cleanTargetFile;
+    args.refTargetFile = commonArgs.cleanTargetFile;
     await switchToRandomTarget(args, false);
     const output = await runWithOutdatedCache(args);
     expect(output).toContain(`Add 3 new translations`);
