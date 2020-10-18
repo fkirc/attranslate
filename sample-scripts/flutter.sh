b75d65c0e8631e246469dde4fc51fdd1351eafb2
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-18 19:06:23 +0200

    mv flutter cache

--- sample-scripts/flutter.sh
+++ sample-scripts/flutter.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-# This example translates an english arb-file into spanish and german. It uses Google Cloud Translate.
-CACHE_DIR="translate-cache"
+# This example translates an english ARB-file into spanish and german. It uses Google Cloud Translate.
 SERVICE_ACCOUNT_KEY="../gcloud/gcloud_service_account.json"
-COMMON_ARGS=( "--srcFile=flutter/lib/l10n/intl_messages.arb" "--srcLng=en" "--srcFormat=flutter-arb" "--targetFormat=flutter-arb" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=$CACHE_DIR" )
+COMMON_ARGS=( "--srcFile=flutter/lib/l10n/intl_messages.arb" "--srcLng=en" "--srcFormat=flutter-arb" "--targetFormat=flutter-arb" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=flutter" )
 
 # Run "npm install --global attranslate" before you try this example.
 attranslate "${COMMON_ARGS[@]}" --targetFile='flutter/lib/l10n/intl_es.arb' --targetLng='es'
