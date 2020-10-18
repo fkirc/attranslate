7d2dff6a5817e8bbf57be0c473dffb5ffcd5eb0a
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-18 18:21:22 +0200

    New simple-script

--- sample-scripts/simple_translate.sh
+++ sample-scripts/simple_translate.sh
@@ -5,4 +5,4 @@ set -e
 # Once you have a service key, you can change "manual" to "google-translate".
 
 # Run "npm install --global attranslate" before you try this example.
-attranslate --srcFile='en/fruits.json' --srcLng='en' --srcFormat='nested-json' --targetFile='de/fruits.json' --targetLng='de' --targetFormat='nested-json' --service='manual'
+attranslate --srcFile='json-raw/fruits-en.json' --srcLng='en' --srcFormat='nested-json' --targetFile='json-raw/fruits-de.json' --targetLng='de' --targetFormat='nested-json' --service='manual'
