225f422426cf31e330bf247b8ff85f20b05f7d67
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-18 19:01:14 +0200

    mv cache

--- sample-scripts/multi_translate.sh
+++ sample-scripts/multi_translate.sh
@@ -2,10 +2,9 @@
 set -e
 
 # This example translates an english JSON-file into spanish, chinese and german. It uses Google Cloud Translate.
-CACHE_DIR="translate-cache"
 BASE_DIR="json-manual-review"
 SERVICE_ACCOUNT_KEY="../gcloud/gcloud_service_account.json"
-COMMON_ARGS=( "--srcFile=$BASE_DIR/en/fruits.json" "--srcLng=en" "--srcFormat=nested-json" "--targetFormat=nested-json" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=$CACHE_DIR" "--matcher=i18next" )
+COMMON_ARGS=( "--srcFile=$BASE_DIR/en/fruits.json" "--srcLng=en" "--srcFormat=nested-json" "--targetFormat=nested-json" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=$BASE_DIR" "--matcher=i18next" )
 
 # Run "npm install --global attranslate" before you try this example.
 attranslate "${COMMON_ARGS[@]}" --targetFile=$BASE_DIR/es/fruits.json --targetLng=es
