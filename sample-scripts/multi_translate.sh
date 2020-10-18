292ff15e062ca7fc6989242794dc11c37beeb880
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-18 21:51:19 +0200

    manualReview=true

--- sample-scripts/multi_translate.sh
+++ sample-scripts/multi_translate.sh
@@ -4,7 +4,7 @@ set -e
 # This example translates an english JSON-file into spanish, chinese and german. It uses Google Cloud Translate.
 BASE_DIR="json-manual-review"
 SERVICE_ACCOUNT_KEY="../gcloud/gcloud_service_account.json"
-COMMON_ARGS=( "--srcFile=$BASE_DIR/en/fruits.json" "--srcLng=en" "--srcFormat=nested-json" "--targetFormat=nested-json" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=$BASE_DIR" "--matcher=i18next" )
+COMMON_ARGS=( "--srcFile=$BASE_DIR/en/fruits.json" "--srcLng=en" "--srcFormat=nested-json" "--targetFormat=nested-json" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--manualReview=true" "--cacheDir=$BASE_DIR" "--matcher=i18next" )
 
 # Run "npm install --global attranslate" before you try this example.
 attranslate "${COMMON_ARGS[@]}" --targetFile=$BASE_DIR/es/fruits.json --targetLng=es
