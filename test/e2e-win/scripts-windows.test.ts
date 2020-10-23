4f0577b95b4f54e34612a34f41448de72c028ce4
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-24 01:54:54 +0200

    Test fix

--- test/e2e-win/scripts-windows.test.ts
+++ test/e2e-win/scripts-windows.test.ts
@@ -1,6 +1,9 @@
 import { runSampleScript } from "../e2e/scripts-e2e-util";
 
 test("simple_translate_windows", async () => {
+  if (process.platform !== "win32") {
+    return;
+  }
   const output = await runSampleScript(`simple_translate_windows.bat`, [
     "json-raw",
   ]);
