c00e3cafa45d2c1ee0e97c83696e6313ca548bc1
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-24 17:13:18 +0200

    flat yml implementation

--- src/file-formats/yaml/yaml-generic.ts
+++ src/file-formats/yaml/yaml-generic.ts
@@ -4,13 +4,34 @@ import {
   WriteTFileArgs,
 } from "../file-format-definitions";
 import { TSet } from "../../core/core-definitions";
+import { readUtf8File, writeUf8File } from "../../util/util";
+import { Options, parse, stringify } from "yaml";
 
 export class YamlGeneric implements TFileFormat {
   readTFile(args: ReadTFileArgs): Promise<TSet> {
-    return Promise.resolve(new Map());
+    const ymlString = readUtf8File(args.path);
+    const options: Options = {
+      keepCstNodes: true,
+      keepNodeTypes: true,
+      prettyErrors: true,
+    };
+    const tSet: TSet = new Map();
+    const obj: Record<string, unknown> = parse(ymlString, options);
+    for (const key of Object.keys(obj)) {
+      const value = obj[key];
+      if (typeof value === "string") {
+        tSet.set(key, value);
+      }
+    }
+    console.log(obj);
+    return Promise.resolve(tSet);
   }
 
   writeTFile(args: WriteTFileArgs): void {
-    throw Error("not implemented");
+    const options: Options = {
+      mapAsMap: true,
+    };
+    const ymlString = stringify(args.tSet, options);
+    writeUf8File(args.path, ymlString);
   }
 }
