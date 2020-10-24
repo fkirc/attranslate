d9c1cbd6b781bc53417505bea8e63727f5881447
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-24 22:07:51 +0200

    Prepare nested yml sample

--- sample-scripts/yaml_ecommerce.sh
+++ sample-scripts/yaml_ecommerce.sh
@@ -8,3 +8,6 @@ COMMON_ARGS=( "--srcFile=yaml/en_ecommerce.yml" "--srcLng=en" "--srcFormat=yaml"
 # Run "npm install --global attranslate" before you try this example.
 attranslate "${COMMON_ARGS[@]}" --targetFile=yaml/es_ecommerce.yml --targetLng=es --manualReview=true
 attranslate "${COMMON_ARGS[@]}" --targetFile=yaml/de_ecommerce.yml --targetLng=de
+
+# Convert a JSON to YML (just for the sake of test-coverage)
+attranslate --srcFile="yaml/nested-fruits.json" --srcFormat=nested-json --srcLng=x --targetFile=yaml/nested-fruits.yml --targetFormat=yaml --targetLng=x --service=sync-without-translate --cacheDir=yaml
