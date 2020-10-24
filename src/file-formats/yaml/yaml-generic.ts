25c03b7e599908087a6774890afb6c22145f8438
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-24 23:54:15 +0200

    yaml switch target lng

--- src/file-formats/yaml/yaml-generic.ts
+++ src/file-formats/yaml/yaml-generic.ts
@@ -4,7 +4,7 @@ import {
   WriteTFileArgs,
 } from "../file-format-definitions";
 import { TSet } from "../../core/core-definitions";
-import { readUtf8File, writeUf8File } from "../../util/util";
+import { logFatal, readUtf8File, writeUf8File } from "../../util/util";
 import {
   Document,
   Options,
@@ -16,6 +16,14 @@ import { FormatCache } from "../common/format-cache";
 import Parsed = Document.Parsed;
 import { flatten, unflatten } from "../../util/flatten";
 import { readJsonProp, writeJsonProp } from "../common/json-common";
+import { Pair, Scalar, YAMLMap } from "yaml/types";
+
+interface YmlWriteContext {
+  args: WriteTFileArgs;
+  doc: Parsed;
+  currentPairs: Array<Pair>;
+  currentJson: Record<string, unknown>;
+}
 
 const documentCache = new FormatCache<unknown, Parsed>();
 
@@ -53,26 +61,71 @@ export class YamlGeneric implements TFileFormat {
   }
 
   writeTFile(args: WriteTFileArgs): void {
-    const doc = documentCache.lookupAuxdata({ path: args.path });
-    let ymlString: string;
-    if (doc) {
-      ymlString = this.createCachedYml(args, doc);
-    } else {
-      ymlString = this.createUncachedYml(args);
-    }
-    writeUf8File(args.path, ymlString);
-  }
-
-  createCachedYml(args: WriteTFileArgs, doc: Parsed): string {
-    return doc.toString();
-  }
-
-  createUncachedYml(args: WriteTFileArgs): string {
     const flatJson: Record<string, string | null> = {};
     args.tSet.forEach((value, key) => {
       writeJsonProp(flatJson, key, value, args);
     });
     const nestedJson = unflatten(flatJson);
+    const doc = documentCache.lookupAuxdata({ path: args.path });
+    let ymlString: string;
+    if (doc) {
+      ymlString = this.createCachedYml(args, doc, nestedJson);
+    } else {
+      ymlString = this.createUncachedYml(args, nestedJson);
+    }
+    writeUf8File(args.path, ymlString);
+  }
+
+  createCachedYml(
+    args: WriteTFileArgs,
+    doc: Parsed,
+    nestedJson: Record<string, unknown>
+  ): string {
+    if (!doc.contents) {
+      logFatal("no cached yml contents");
+    }
+    const contents: Partial<YAMLMap> = doc.contents as Partial<YAMLMap>;
+    if (!contents.items || !Array.isArray(contents.items)) {
+      logFatal("no cached yml items");
+    }
+    const writeContext: YmlWriteContext = {
+      args,
+      doc,
+      currentPairs: contents.items,
+      currentJson: nestedJson,
+    };
+    this.recursiveReplace(writeContext);
+    return doc.toString();
+  }
+
+  recursiveReplace(writeContext: YmlWriteContext) {
+    writeContext.currentPairs.forEach((pair) => {
+      const childJson = writeContext.currentJson[pair.key?.value];
+      if (childJson === undefined) {
+        return;
+      }
+      const value: YAMLMap | Scalar = pair.value;
+      if (
+        value.type === "MAP" ||
+        (value.type === "FLOW_MAP" && typeof childJson === "object")
+      ) {
+        const childContext: YmlWriteContext = {
+          ...writeContext,
+          currentJson: childJson as Record<string, unknown>,
+          currentPairs: pair.value.items,
+        };
+        this.recursiveReplace(childContext);
+      } else if (typeof childJson === "string" || childJson === null) {
+        (value as Scalar).value = childJson ?? "";
+      }
+    });
+    console.log(writeContext);
+  }
+
+  createUncachedYml(
+    args: WriteTFileArgs,
+    nestedJson: Record<string, unknown>
+  ): string {
     const options: Options = {
       mapAsMap: false,
     };
