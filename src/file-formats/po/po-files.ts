384e9ef22cd9c3681d4e3ccba023ddc073df292b
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-26 15:49:14 +0100

    longestLineLen heuristic

--- src/file-formats/po/po-files.ts
+++ src/file-formats/po/po-files.ts
@@ -7,44 +7,49 @@ import { TSet } from "../../core/core-definitions";
 import { readUtf8File, writeUf8File } from "../../util/util";
 import { GetTextTranslations, po } from "gettext-parser";
 import { FormatCache } from "../common/format-cache";
-import { poParse, PoParseContext } from "./po-parse";
+import { longestLineLen, poParse, PoParseContext } from "./po-parse";
 
-const potCache = new FormatCache<unknown, GetTextTranslations>();
+interface PotAuxData {
+  potFile: GetTextTranslations;
+  rawFile: string;
+}
+const potCache = new FormatCache<unknown, PotAuxData>();
 
 export class PoFile implements TFileFormat {
   readTFile(args: ReadTFileArgs): Promise<TSet> {
-    const raw = readUtf8File(args.path);
+    const rawFile = readUtf8File(args.path);
     const context: PoParseContext = {
       args,
-      raw,
+      raw: rawFile,
     };
     const { tSet, potFile } = poParse(context);
     potCache.insertFileCache({
       path: args.path,
       entries: new Map(),
-      auxData: potFile,
+      auxData: { potFile, rawFile },
     });
     return Promise.resolve(tSet);
   }
 
   writeTFile(args: WriteTFileArgs): void {
-    const cachedPot = potCache.getOldestAuxdata();
+    const auxData = potCache.getOldestAuxdata();
     let output: string;
-    if (!cachedPot) {
+    if (!auxData) {
       throw Error("uncached pot not implemented");
     } else {
-      output = writeCachedPot(cachedPot);
+      output = writeCachedPot(auxData);
     }
     writeUf8File(args.path, output);
     potCache.purge();
   }
 }
 
-function writeCachedPot(cachedPot: GetTextTranslations): string {
+function writeCachedPot(auxData: PotAuxData): string {
+  const lineLen = longestLineLen(auxData.rawFile);
   const options = {
-    foldLength: 100,
+    foldLength: lineLen,
     sort: false,
   };
-  const buffer = po.compile(cachedPot, options);
+  const buffer = po.compile(auxData.potFile, options);
   return buffer.toString("utf-8");
 }
--- src/file-formats/po/po-parse.ts
+++ src/file-formats/po/po-parse.ts
@@ -8,6 +8,17 @@ export interface PoParseContext {
   raw: string;
 }
 
+export function longestLineLen(str: string) {
+  const lines = str.split("\n");
+  let longestLine = 0;
+  lines.forEach((line) => {
+    if (line.length >= longestLine) {
+      longestLine = line.length;
+    }
+  });
+  return longestLine;
+}
+
 export function poParse(
   context: PoParseContext
 ): { potFile: GetTextTranslations; tSet: TSet } {
