a7a81c342ad31b8222f66216f2e0c815fbadfd86
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-26 18:24:09 +0100

    PO test

---
+++ sample-scripts/po_generic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# This example translates an english PO-file into spanish and german. It uses Google Cloud Translate.
+SERVICE_ACCOUNT_KEY="../gcloud/gcloud_service_account.json"
+BASE_DIR=po-generic
+COMMON_ARGS=( "--srcFile=$BASE_DIR/en.po" "--srcLng=en" "--srcFormat=po" "--targetFormat=po" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=$BASE_DIR" )
+
+# Run "npm install --global attranslate" before you try this example.
+attranslate "${COMMON_ARGS[@]}" --targetFile=$BASE_DIR/es.po --targetLng=es
+attranslate "${COMMON_ARGS[@]}" --targetFile=$BASE_DIR/de.po --targetLng=de --manualReview=true
+
+# Convert a JSON to PO (just for the sake of test-coverage)
+attranslate --srcFile="$BASE_DIR/nested-fruits.json" --srcFormat=nested-json --srcLng=x --targetFile=$BASE_DIR/nested-fruits.po --targetFormat=po --targetLng=x --service=sync-without-translate --cacheDir=$BASE_DIR
