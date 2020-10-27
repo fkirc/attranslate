84b47ba83066d9d4517a27765c3207cca7905d36
Felix Kirchengast <mail@felixkirchengast.at>
2020-10-27 01:12:26 +0100

    rf flutter

--- sample-scripts/flutter.sh
+++ sample-scripts/flutter.sh
@@ -2,9 +2,10 @@
 set -e
 
 # This example translates an english ARB-file into spanish and german. It uses Google Cloud Translate.
+BASE_DIR=flutter/lib/l10n
 SERVICE_ACCOUNT_KEY="../gcloud/gcloud_service_account.json"
-COMMON_ARGS=( "--srcFile=flutter/lib/l10n/intl_messages.arb" "--srcLng=en" "--srcFormat=arb" "--targetFormat=arb" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=flutter" )
+COMMON_ARGS=( "--srcFile=$BASE_DIR/intl_messages.arb" "--srcLng=en" "--srcFormat=arb" "--targetFormat=arb" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=flutter" )
 
 # Run "npm install --global attranslate" before you try this example.
-attranslate "${COMMON_ARGS[@]}" --targetFile='flutter/lib/l10n/intl_es.arb' --targetLng='es' --manualReview='true'
-attranslate "${COMMON_ARGS[@]}" --targetFile='flutter/lib/l10n/intl_de.arb' --targetLng='de'
+attranslate "${COMMON_ARGS[@]}" --targetFile=$BASE_DIR/intl_es.arb --targetLng=es --manualReview=true
+attranslate "${COMMON_ARGS[@]}" --targetFile=$BASE_DIR/intl_de.arb --targetLng=de
