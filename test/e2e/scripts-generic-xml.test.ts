d0482de86608b17587f8885cdc146be64a9f095f
Felix Kirchengast <mail@felixkirchengast.at>
2020-11-05 17:55:20 +0100

    xml testing

---
+++ test/e2e/scripts-generic-xml.test.ts
@@ -0,0 +1,61 @@
+import {
+  injectPrefixLines,
+  removeLines,
+  runSampleScript,
+  sampleDir,
+} from "./scripts-e2e-util";
+import { joinLines } from "../test-util/test-util";
+import { unlinkSync } from "fs";
+import { join } from "path";
+import { getDebugPath } from "../../src/util/util";
+
+const assetDir = "xml-generic";
+const testScript = "./xml_generic.sh";
+const mainTarget = join(assetDir, "de.xml");
+const nonCachedTarget = join(assetDir, "nested-fruits.xml");
+const targetPaths: string[] = [
+  join(assetDir, "ar.xml"),
+  mainTarget,
+  nonCachedTarget,
+];
+
+test("xml clean", async () => {
+  const output = await runSampleScript(testScript, [assetDir]);
+  expect(output).toBe(
+    joinLines(
+      targetPaths.map((path) => {
+        return `Target is up-to-date: '${path}'`;
+      })
+    )
+  );
+});
+
+test("xml re-create non-cached target", async () => {
+  unlinkSync(join(sampleDir, nonCachedTarget));
+  const output = await runSampleScript(testScript, [assetDir]);
+  expect(output).toContain(
+    `Write target ${getDebugPath(join(sampleDir, nonCachedTarget))}`
+  );
+});
+
+test("xml delete stale translations", async () => {
+  injectPrefixLines({
+    path: join(sampleDir, mainTarget),
+    lines: ["<InvalidOuter></InvalidOuter>"],
+  });
+  const output = await runSampleScript(testScript, [assetDir]);
+  expect(output).toContain(`Delete 1 stale translations`);
+});
+
+test("xml insert new translations", async () => {
+  removeLines({
+    path: join(sampleDir, mainTarget),
+    linesToRemove: [
+      '                    <else someAtt="x">Inhalt</else>',
+      "    <InnerElse>Dritte</InnerElse>",
+    ],
+  });
+  const output = await runSampleScript(testScript, [assetDir]);
+  expect(output).toContain(`Add 2 new translations`);
+  expect(output).toContain(`Delete 1 stale translations`);
+});
