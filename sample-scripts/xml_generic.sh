ac5545168071d9a46041d4df6172e28cd67b506a
Felix Kirchengast <mail@felixkirchengast.at>
2020-11-05 17:03:07 +0100

    update generic sample

--- sample-scripts/xml_generic.sh
+++ sample-scripts/xml_generic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# This example translates an english PO-file into spanish and german. It uses Google Cloud Translate.
+# Beside of Android-XMLs, we can translate arbitrary (nested) XMLs that have their translatable content within closed tags like so: <Tag>content to translate</Tag>
 SERVICE_ACCOUNT_KEY="gcloud/gcloud_service_account.json"
 BASE_DIR=xml-generic
 COMMON_ARGS=( "--srcFormat=xml" "--targetFormat=xml" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" "--cacheDir=$BASE_DIR" )
 
 # Run "npm install --global attranslate" before you try this example.
 attranslate "${COMMON_ARGS[@]}" --srcFile=$BASE_DIR/en.xml --srcLng=en --targetFile=$BASE_DIR/ar.xml --targetLng=ar
-attranslate "${COMMON_ARGS[@]}" --srcFile=$BASE_DIR/en.xml --srcLng=en --targetFile=$BASE_DIR/de.xml --targetLng=de --manualReview=true
+attranslate "${COMMON_ARGS[@]}" --srcFile=$BASE_DIR/en.xml --srcLng=en --targetFile=$BASE_DIR/de.xml --targetLng=de
