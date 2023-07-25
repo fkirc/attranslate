8ac18db53507e06f2e70b95249969eb1b1254090
Felix Kirc <mail@felixkirchengast.at>
2023-07-26 00:04:59 +0200

    add a search/replace script

---
+++ sample-scripts/search_replace.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+/**
+ * This is a sample-script for preprocessing and postprocessing of transation-files.
+ * Preprocessing or postprocessing may be needed because attranslate cannot cover every edge-case.
+ */
+
+function searchAndReplaceInFile(filePath, searchRegex, replacement) {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return;
+    }
+
+    const updatedContent = data.replace(searchRegex, replacement);
+    if (updatedContent === data) {
+      console.log("warning: did not replace anything in " + filePath);
+      return;
+    }
+
+    fs.writeFile(filePath, updatedContent, "utf8", (err) => {
+      if (err) {
+        console.error("Error writing to file:", err);
+        return;
+      }
+    });
+  });
+}
+
+// Check if all three required arguments are provided
+if (process.argv.length !== 5) {
+  console.error(
+    "Usage: node search_replace.js <file_path> <search_regex> <replacement>"
+  );
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const searchRegex = new RegExp(process.argv[3], "g");
+const replacement = process.argv[4];
+
+searchAndReplaceInFile(filePath, searchRegex, replacement);
--- sample-scripts/yaml_ecommerce.sh
+++ sample-scripts/yaml_ecommerce.sh
@@ -6,8 +6,14 @@ SERVICE_ACCOUNT_KEY="gcloud/gcloud_service_account.json"
 COMMON_ARGS=( "--srcFile=yaml/en_ecommerce.yml" "--srcLng=en" "--srcFormat=yaml" "--targetFormat=yaml" "--service=manual" "--serviceConfig=$SERVICE_ACCOUNT_KEY" )
 
 # Run "npm install --global attranslate" before you try this example.
+
+node search_replace.js "yaml/es_ecommerce.yml" "es:" "en:\n" # preprocessing
 attranslate "${COMMON_ARGS[@]}" --targetFile=yaml/es_ecommerce.yml --targetLng=es
+node search_replace.js "yaml/es_ecommerce.yml" "en:" "es:\n" # postprocessing
+
+node search_replace.js "yaml/de_ecommerce.yml" "de:\n" "en:\n" # preprocessing
 attranslate "${COMMON_ARGS[@]}" --targetFile=yaml/de_ecommerce.yml --targetLng=de
+node search_replace.js "yaml/de_ecommerce.yml" "en:\n" "de:\n" # postprocessing
 
 # Convert a JSON to YML (just for the sake of test-coverage)
 attranslate --srcFile="yaml/nested-fruits.json" --srcFormat=nested-json --srcLng=x --targetFile=yaml/nested-fruits.yml --targetFormat=yaml --targetLng=x --service=sync-without-translate
