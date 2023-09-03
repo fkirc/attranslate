f523441d02ca1b54650a49f601b2efa9d1f89148
Felix Kirc <mail@felixkirchengast.at>
2023-09-03 21:15:23 +0200

    add typechat sample

--- sample-scripts/xml_generic.sh
+++ sample-scripts/xml_generic.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-# Beside of Android-XMLs, we can translate arbitrary (nested) XMLs that have their translatable content within closed tags like so: <Tag>content to translate</Tag>
-SERVICE_ACCOUNT_KEY="gcloud/gcloud_service_account.json"
+# translate arbitrary (nested) XMLs that have their translatable content within closed tags like so: <Tag>content to translate</Tag>
 BASE_DIR=xml-generic
-COMMON_ARGS=( "--srcFormat=xml" "--targetFormat=xml" "--service=google-translate" "--serviceConfig=$SERVICE_ACCOUNT_KEY" )
+COMMON_ARGS=( "--srcFormat=xml" "--targetFormat=xml" "--service=typechat" )
 
 # Run "npm install --global attranslate" before you try this example.
 attranslate "${COMMON_ARGS[@]}" --srcFile=$BASE_DIR/en.xml --srcLng=en --targetFile=$BASE_DIR/ar.xml --targetLng=ar
